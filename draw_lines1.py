5d0f4e89c22337850d0a315a22ea9b0f9d375d8b
cbg <email@example.com>
2024-05-06 23:02:21 +0900

    1

--- draw_lines1.py
+++ draw_lines1.py
@@ -1,4 +1,3 @@
-import sys
 from math import sin, cos, radians
 import pygame
 from pygame.locals import QUIT
@@ -12,7 +11,6 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                sys.exit()
         SURFACE.fill((0, 0, 0))
         
         pointlist0, pointlist1 = [], []
@@ -20,13 +18,11 @@ def main():
             rad = radians(theta)
             pointlist0.append((cos(rad)*100 + 100, sin(rad)*100 + 150))
             pointlist1.append((cos(rad)*100 + 300, sin(rad)*100 + 150))
-            print(cos(rad)*100, sin(rad)*100)
             
-        pygame.draw.lines(SURFACE, (255, 255, 255), True, pointlist0, 5)
+        pygame.draw.lines(SURFACE, 'red', True, pointlist0, 5)
         pygame.draw.aalines(SURFACE, (255, 255, 255), True, pointlist1, 0)
         
         pygame.display.update()
         FPSCLOCK.tick(1)
 
-if __name__ == '__main__':
-    main()
+main()
