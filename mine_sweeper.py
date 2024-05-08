552e8a0ed28acbe182c9ac841ac6f4453b2fc8d1
cbg5946 <106308669+cbg5946@users.noreply.github.com>
2024-05-08 20:00:46 +0900

    Add files via upload

---
+++ mine_sweeper.py
@@ -0,0 +1,49 @@
+from math import floor
+from random import randint
+import pygame
+from pygame import QUIT, MOUSEBUTTONDOWN
+
+WIDTH = 20
+HEIGHT = 15
+SIZE = 50
+NUM_OF_BOMBS = 20
+EMPTY = 0
+BOMB = 1
+OPENED = 2
+OPEN_COUNT = 0
+CHECKED = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+pygame.init()
+SURFACE = pygame.display.set_mode([WIDTH*SIZE, HEIGHT*SIZE])
+FPSCLOCK = pygame.time.Clock()
+
+def num_of_bomb(field, x_pos, y_pos):
+    count = 0 # 0개
+    for yoffset in range(-1, 2):
+        for xoffset in range(-1, 2):
+            xpos, ypos = (x_pos + xoffset, y_pos + yoffset)
+            if 0 <= xpos < WIDTH and 0 <= ypos < HEIGHT and field[ypos][xpos] == BOMB:
+                count += 1
+    return count
+
+def open_tile(field, x_pos, y_pos):
+    # 타일 오픈
+    global OPEN_COUNT
+    if CHECKED[y_pos][x_pos]:
+        return
+    
+    CHECKED[y_pos][x_pos] = True
+    for yoffset in range(-1, 2):
+        for xoffset in range(-1, 2):
+            xpos, ypos = (x_pos + xoffset, y_pos + yoffset)
+            if 0 <= xpos < WIDTH and 0 <= ypos < HEIGHT and field[ypos][xpos] == EMPTY:
+                field[ypos][xpos] - OPENED
+                OPEN_COUNT += 1
+                count = num_of_bomb(field, xpos, ypos)
+                if count == 0 and not (xpos == x_pos and ypos == y_pos):
+                    open_tile(field, xpos, ypos)
+
+def main():
+    smallfont = pygame.font.SysFont(None, 36)
+    largefont = pygame.font.SysFont(None, 72)
+    message_clear = largefont.render("!!CLEARED!!")
